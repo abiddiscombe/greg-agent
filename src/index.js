f114bf7f6c513917349afb22e1e5bdbd7171b11f
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2022-10-09 12:41:08 +0100

    add-cors-wildcard-support

--- src/index.js
+++ src/index.js
@@ -11,6 +11,12 @@ import express from 'express'
 
 const app = express()
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 const config = {
     port: process.env.SVR_PORT || 3000,
     greg: {
